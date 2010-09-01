77a2c70a748b81f0ddd850d8479d6ee1caa25c2f
Mark Philpot <mark.philpot@gmail.com>
2010-09-01 03:20:39 +0000

    Setup apache and admin

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 's7g3@6mm=w1310rig91e6-^zit0r=8=#z+_0ce#q78xec80^&j'
