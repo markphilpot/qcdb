56618e2f90a5bbac8c0cc3a811f686d51787a90b
Mark Philpot <mark.philpot@gmail.com>
2010-09-03 16:53:21 +0000

    Mods to admin

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from qcdb.web.models import Comic
+from qcdb.web.models import Character
+from qcdb.web.models import Location
+from qcdb.web.models import Event
 from django.contrib import admin
 
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 admin.site.register(Comic)
-admin.site.register(Character)
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(Location)
 admin.site.register(Event)
